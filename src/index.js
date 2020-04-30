f0c3d6c732fb0fa334556d21c56769eedcf4170d
Ty <tyreewilliams515@gmail.com>
2020-04-30 10:26:47 -0400

    part 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-// write your code here
+window.addEventListener('DOMContentLoaded', (event) => {
+    fetch('http://localhost:3000/image')
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(function (element) {
+            imgcrd = document.getElementById('image-card')
+            console.log(imgcrd)
+
+        })
+    })
+})
