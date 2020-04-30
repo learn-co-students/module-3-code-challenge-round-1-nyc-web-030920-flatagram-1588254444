89d25359a8c857b61a7c522e096c318f165162ea
Anil Gurung <anil_grg@hotmail.com>
2020-04-30 08:46:33 -0600

    title and image done

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
 // write your code here
+
+
+
+
+const baseUrl = 'http://localhost:3000/image'
+document.addEventListener('DOMContentLoaded',function(){
+
+    fetchImage()
+
+    getLikes()
+
+})
+
+function fetchImage(){
+    fetch(baseUrl)
+    .then(response => response.json())
+    .then(pic => postPicture(pic))
+    // console.log(pic)
+}
+
+function postPicture(pic){
+    let wallPic = document.querySelector('.image')
+    // debugger
+    let titlePic = document.querySelector('.title')
+    // console.log(wallpic)
+    wallPic.src = pic.image
+    titlePic.innerText = pic.title
+    // debugger
+    // debugger
+}
+
+function getLikes(){
+    
+
+
+}
+
+
