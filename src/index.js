806a9c1d8f4c04aa475a453f40d336af634393f0
Eliot Henson <eliotrhenson@gmail.com>
2020-04-30 10:46:03 -0400

    stuck

--- src/index.js
+++ src/index.js
@@ -14,37 +14,40 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(showImages)
     }
 
-    const showImages = images => {
-        imageCard.innerHTML = ' '
-        let imageCard = document.getElementsByClassName("image-card")[0]
-        console.log(imageCard)
+    // const showImages = images => {
+    //     let imageCard = document.getElementsByClassName("image-card")[0]
+    //     imageCard.innerHTML = ' '
+    //     console.log(imageCard)
 
-        images.forEach(image => {
-            imageCard.innerHTMl = `
-            <h2 class="title">${image.title}</h2>
-            <img src= ${image.image} class="image" />
-            <div class="likes-section">
-            <span class="likes"> ${image.likes}likes</span>
-            <button class="like-button">♥</button>
-            </div>
-            <ul class="comments">
-            <li>Get rid of these comments</li>
-            <li>And replace them with the real ones</li>
-            <li>From the server</li>
-            </ul>
-            <form class="comment-form">
-            <input
-            class="comment-input"
-            type="text"
-            name="comment"
-            placeholder="Add a comment..."
-            />
-            <button class="comment-button" type="submit">Post</button>
-            </form>
-            `
+    //     console.log(image.title)
 
-        })
-    }
+            // let card = `
+            // <h2 class="title">${image.title}</h2>
+            // <img src= ${image.image} class="image" />
+            // <div class="likes-section">
+            // <span class="likes"> ${image.likes}likes</span>
+            // <button class="like-button">♥</button>
+            // </div>
+            // <ul class="comments">
+            // <li> ${image.comments[0]} </li>
+            // <li> ${image.comments[1]} </li>
+            // <li> ${image.comments[2]} </li>
+            // </ul>
+            // <form class="comment-form">
+            // <input
+            // class="comment-input"
+            // type="text"
+            // name="comment"
+            // placeholder="Add a comment..."
+            // />
+            // <button class="comment-button" type="submit">Post</button>
+            // </form>
+            // `
+
+            // imageCard.append(card)
+
+
+    // }
 
 
     getImages()
