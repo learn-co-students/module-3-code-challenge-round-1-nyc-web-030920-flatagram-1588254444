fc2a391535f15c35c41b6c4614b04d9aac4ec77d
stephenverdi95 <stephenverdi152@gmail.com>
2020-04-30 10:26:14 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 // write your code here
+//function getData
+//get fetch request and display in DOM within getData
+//display function withing getData
+//also get rid of old text in display function
+//function likePic
+//send patch fetch request and call getData within likePic
+//function addComment
+//no fetch just add to DOM
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('test')
+
+    function getData() {
+        function displayFunction(data) {
+            console.log(data)
+        }
+        fetch('http://localhost:3000')
+        .then(response => response.json)
+        .then(data => displayFunction(data))
+    }
+
+    getData()
+})
