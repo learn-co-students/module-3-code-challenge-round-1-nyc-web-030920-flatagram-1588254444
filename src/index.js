49bf75718c796717ffb554a5aaccca79540c2786
Anil Gurung <anil_grg@hotmail.com>
2020-04-30 09:27:52 -0600

    comment

--- src/index.js
+++ src/index.js
@@ -7,8 +7,15 @@ const baseUrl = 'http://localhost:3000/image'
 document.addEventListener('DOMContentLoaded',function(){
 
     fetchImage()
+   
+    fetchLikes()
 
-    getLikes()
+
+    fetchComments()
+
+    addLike()
+
+    addComment()
 
 })
 
@@ -30,10 +37,94 @@ function postPicture(pic){
     // debugger
 }
 
-function getLikes(){
-    
+function fetchLikes(){
+    fetch(baseUrl)
+    .then(response =>response.json())
+    .then(pic => postLikes(pic))
+}
+
+function postLikes(pic){
+    let likes = document.querySelector('.likes')
+    // debugger
+    // console.log(likes)
+    // debugger
+    likes.innerText = pic.likes
+}
+
+function addLike(){
+    let likeBtn = document.querySelector('.like-button')
+    //  console.log(likeBtn)
+
+    let likes = documet.querySelector('.likes')
+
+    likeBtn.addEventListener('click',function(e){
+        e.preventDefault()
+        let likeInt = parseInt(likes.innerHTML)
+        likeInt += 1
+        likes.innerText = likeInt
+
+        let newLikes = {
+            image_id: imageId
+        }
+
+        fetch(baseUrl,{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept':'application/json'
+            },
+            body: JSON.stringify(newLikes)
+        })
+    })
 
 
 }
 
+function fetchComments(){
+    fetch(baseUrl)
+    .then(resp => resp.json())
+    .then(pic => postComments(pic))
 
+}
+
+function postComments(pic){
+    let commentWall = document.querySelector('.comments')
+    pic.comments.forEach(function(comment){
+        let existingCom = document.createElement('li')
+        existingCom.innerText = comment.content
+        commentWall.append(existingCom)
+    })
+}
+
+
+
+function addComment(){
+    let commentForm = document.querySelector('.comment-form')
+
+    commentForm.addEventListener('submit', function(e){
+        e.preventDefault();
+
+        let commentInput = document.querySelector('.comment-input')
+        let commentWall = document.querySelector('.comments')
+
+        let comment = document.createElement('li')
+        comment.innerText = commentInput.nodeValue
+
+        commentWall.append(comment)
+
+        fetch(baseUrl,{
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept':'application/json'
+            },
+            body: JSON.stringify({
+                content: commentInput.value
+            })
+
+
+        }
+            )
+
+    })
+}
