84d4381e1e155b4ce72c5ae93eb02008af70b63f
Yahyagok <ygok17@hotmail.com>
2020-04-30 11:03:51 -0400

    second one

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     function  getImage(){ 
     fetch("http://localhost:3000/image")
     .then(r => r.json())
-    // .then(image => increaseLike(image))
     }
 
 
@@ -22,13 +21,40 @@ let likeButton = document.getElementsByClassName("like-button")[0]
 likeButton.addEventListener('click',increaseLike )
 
 function increaseLike(event) {
-   let span = document.getElementsByClassName('likes')[0]
-   console.log( span.innerHTML = parseInt(span.innerHTML)+1 )
+    let span = document.getElementsByClassName('likes')[0]
+    span.innerHTML = parseInt(span.innerHTML)+1 
+    const likes = span.innerHTML
 
-    
-    
+
+    fetch('http://localhost:3000/image',{
+        method: "PATCH", 
+        header: {
+        'Content-Type': 'application/json',  
+        'Accept': 'application/json'
+        },
+          body: JSON.stringify({likes})
+    } )
 
 }
+
+
+    const ul = document.getElementsByClassName('comments')[0]
+     ul.innerHTML = `<li>What a cute dog!</li>
+     <li>He has a nose for this!</li>
+     <li>Woof!</li>`
+    
+    const image = document.getElemen
+
+
+
+
+
+
+
+    
+    
+
+
 
 
 
