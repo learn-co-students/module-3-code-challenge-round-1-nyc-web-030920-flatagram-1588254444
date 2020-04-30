618d5854a0e07d51932440112d138203b2403bf7
Vivica <Ma>
2020-04-30 11:28:56 -0400

    not doing good at all

--- src/index.js
+++ src/index.js
@@ -1,1 +1,49 @@
-// write your code here
+// // write your code here
+//√ See the image received from the server, including its likes and comments when the page loads
+// Click on the heart icon to increase image likes, and still see them when I reload the page
+// Add a comment (no persistence needed)
+
+
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    const url = "http://localhost:3000/image"
+
+    getImage()
+   
+    function getImage(){
+        return fetch(url)
+        .then(r=> r.json())
+        .then(image => {      
+            console.log(image.title)
+            const imgCard = document.getElementsByClassName("image-card")
+            const h2 = document.getElementsByClassName("title")
+            h2.innertext = `${image.title}`
+            const img = document.getElementsByClassName("image")
+            img.src = "${image.image}"
+            const ul = document.getElementsByClassName("comments")
+            console.log(ul)
+           
+            ul.innertext = `
+                ${image.comments}.forEach(function(comment){
+                comment.content
+            `
+            document.addEventListener("click", function(event){
+                event.preventDefault()
+                if(event.target.className === "like-button"){
+
+                }
+                
+                if(event.target.className === "commment-button"){
+                    
+
+                }
+                
+            })
+        
+        })
+
+    }
+    
+
+})
