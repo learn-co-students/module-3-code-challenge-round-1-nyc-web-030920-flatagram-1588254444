385ec4dd7b18d732633b751ae028ed22e056116e
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-04-30 11:26:30 -0400

    new features

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@
 //user can add a comment and it displays on the DOM
 
 document.addEventListener('DOMContentLoaded', () => {
-    fetchImage()  
+    fetchImage()
+    increaseLikes()
+    addComments()  
 })
 
 
@@ -27,14 +29,48 @@ const displayImage = (data) => {
 };
 
 const showComments = (comment) => {
-
     const commentContainer = document.querySelector('.comments')
     let li = document.createElement('li') 
     li.textContent = comment.content
     commentContainer.append(li)
 }; 
 
+const increaseLikes = () => {
+    const likeBtn = document.querySelector('.like-button')
+    likeBtn.addEventListener('click', function(event) {
+        let likesCount = parseInt(document.querySelector('.likes').textContent)
+        console.log(likesCount)
+        let newLikes = likesCount++
+        console.log(likesCount)
+        likesCount.innerText = newLikes + ' Likes'
+        
+        saveLikes(likesCount)
+    })
+};
 
+const addComments = () => {
+    const commentContainer = document.querySelector('.comments')
+    const commentForm = document.querySelector('.comment-form')
+    commentForm.addEventListener('submit', function(event) {
+        event.preventDefault()
+        let comment = event.target.comment.value
+        let li = document.createElement('li') 
+        li.textContent = comment
+        commentContainer.append(li)
+    })
+};
+
+const saveLikes = (likesCount) => {
+    fetch('http://localhost:3000/image', {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringity({
+            likes: likesCount
+        })
+    })
+}
 
 
 
