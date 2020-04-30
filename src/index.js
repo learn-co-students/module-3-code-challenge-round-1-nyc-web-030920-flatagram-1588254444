9b4c3a7ea8a97435134b5890c130af0229b29451
bbedevian <brettbedevian@gmail.com>
2020-04-30 10:46:03 -0400

    second commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
-// fetch image info from local host
-// set image container info = to ^
-// find the heart + likes "likes-section"
-// event listener on the heart 
+// √ fetch image info from local host
+// √ set image container info = to ^
+// √ find the heart + likes "likes-section"
+// √ event listener on the heart 
 // patch likes +=1 to DB
 // event listener on the post
 // patch post to DB  
@@ -18,11 +18,12 @@ const imgCard = document.querySelector(".image-card")
 fetch(baseURL)
   .then(response => response.json())
   .then(post => {
+      imgCard.dataset.likes = post.likes
       imgCard.innerHTML = `
         <h2 class="title">${post.title}</h2>
         <img src="${post.image}" class="image" />
         <div class="likes-section">
-          <span class="likes">0 likes</span>
+          <span class="likes">`${post.likes} likes`</span>
           <button class="like-button">♥</button>
         </div>
         <ul class="comments">
@@ -39,18 +40,51 @@ fetch(baseURL)
         </form>
       `
       let comments = post.comments
-      console.log(comments)
         comments.forEach(comment => {
             addComment(comment)
         }) //end of forEach comment
     
   }); // end of Fetch image data
 
+  document.addEventListener("click", function(e){
+      e.preventDefault();
+    
+      if (e.target.className === "like-button"){
+          let likeSection = e.target.parentNode
+          let likes = parseInt(likeSection.querySelector("span").textContent)
+          newLikes = likes +=1
+          
+
+        fetch(baseURL, {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({likes: newLikes})
+          })
+          .then(resp => {
+              console.log(resp)
+          })
+          
+
+
+      } // end of if click is like button
+
+
+
+  }) // end of event listener
+
+
+
+
+  // Functionz-----------------------------
+
   function addComment(comment) {
       let ul = document.querySelector(".comments")
       let li = document.createElement("li")
       li.dataset.id = comment.id
-      li.textContent = comment.content
+      li.textContent = `- ${comment.content}`
+      ul.append(li)
   }
 
 
