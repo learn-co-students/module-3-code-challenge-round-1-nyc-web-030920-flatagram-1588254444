e5bffe646c2da978fabef5e20649c4fcfcab02c9
devmullets <devmullets@gmail.com>
2020-04-30 10:29:19 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-// write your code here
+// click on heart icon to increase likes - persists
+// add a comment - no persist
+
+const flatagramUrl = 'http://localhost:3000/image'
+const newHeaders = {
+    "Content-type": "application/json"
+}
+
+const comments = document.querySelector('.comments')
+const likes = document.querySelector('.likes')
+const image = document.querySelector('.image')
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log('hi buddy :)')
+    const likeBtn = document.querySelector('.like-button')
+
+    eraseComments()
+    loadImage()
+    
+    likeBtn.addEventListener('click', function(event) {
+
+        let likesNum = parseInt(likes.innerText) + 1
+        likes.innerText = likesNum
+
+        let likesObj = {likes: likesNum}
+        updateLikeCount(likesObj)
+
+    })
+
+
+})
+
+function eraseComments () {
+    comments.innerHTML = ''
+}
+
+function loadImage() {
+    fetch(flatagramUrl)
+        .then(response => response.json())
+        .then(load => {
+            image.src = load.image
+            likes.innerText = load.likes
+ 
+            load.comments.forEach(comment => {
+                let li = document.createElement('li')
+                li.innerText = comment.content
+                comments.appendChild(li)
+            })
+        })
+}
+
+function updateLikeCount(likes) {
+    fetch(flatagramUrl, {
+        method: 'PATCH',
+        body: JSON.stringify(likes),
+        headers: newHeaders
+    })
+        .then(response => response.json())
+        .then(console.log)
+}
+
