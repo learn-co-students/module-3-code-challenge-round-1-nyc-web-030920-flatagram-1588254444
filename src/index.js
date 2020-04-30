a2a3c7aa5ddcf7841ff563aa8a400547be176b6a
Yueying <wangyy395@gmail.com>
2020-04-30 11:19:46 -0400

    persistence comment

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
 //√1. fetch info from api, append it to <div class="image-card">
 //√2. like button works and PATCH database with increased like number
 //√3. can comment to a post/ not persistence
+//√4. Still see the comments written after reloading the page
+//5. Downvote an image
+//6. Delete a comment
 const baseUrl = 'http://localhost:3000/image';
 const dogCard = document.querySelector('.image-card');
 let dogObj = {};
@@ -45,6 +48,10 @@ document.addEventListener('DOMContentLoaded', function(){
         comments.forEach(function(comment){
             let li = document.createElement('li')
             li.textContent = comment['content'];
+            let deleteButton = document.createElement('button');
+            deleteButton.textContent = 'delete';
+            deleteButton.setAttribute('class', 'delete-comment');
+            li.append(deleteButton);
             ul.append(li);
         })
     }
@@ -66,13 +73,33 @@ document.addEventListener('DOMContentLoaded', function(){
         } else if (eventTarget.className === 'comment-button') {
             event.preventDefault();
             let comment = eventTarget.parentElement.comment.value;
-            let ul = document.querySelector('.comments');
-            let li = document.createElement('li');
-            li.textContent = comment;
-            ul.append(li);
-            eventTarget.parentElement.reset();
+
+            //non persistence comment code, line 78 - 82
+            // let ul = document.querySelector('.comments');
+            // let li = document.createElement('li');
+            // li.textContent = comment;
+            // ul.append(li);
+            // eventTarget.parentElement.reset();
+            
+            //persistence comment code, line 85 - 97
+            let id = dogObj['comments'].length + 1;
+            let newComment = {'id': id, 'content': comment}
+            dogObj['comments'].push(newComment);
+            fetch(baseUrl, {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    accept: 'application/json'
+                },
+                body: JSON.stringify(dogObj)
+            })
+            .then(res => res.json())
+            .then(fetchDogInfo);
         }
     })
 
     fetchDogInfo()
 })
+
+
+
