40b08e1fcf44d1142b4276a04a49450a3d4c6635
Yahyagok <ygok17@hotmail.com>
2020-04-30 10:27:02 -0400

    first time

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
 // write your code here
+
+// do the fecth and get the info about dog
+// do like function 
+// do post function 
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    getImage()
+
+
+    function  getImage(){ 
+    fetch("http://localhost:3000/image")
+    .then(r => r.json())
+    // .then(image => increaseLike(image))
+    }
+
+
+
+let likeButton = document.getElementsByClassName("like-button")[0]
+
+likeButton.addEventListener('click',increaseLike )
+
+function increaseLike(event) {
+   let span = document.getElementsByClassName('likes')[0]
+   console.log( span.innerHTML = parseInt(span.innerHTML)+1 )
+
+    
+    
+
+}
+
+
+
+
+});
