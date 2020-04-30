ab1704b0a8952317d853461d4db02adbfff575f2
lucassherwin <lksherwin@gmail.com>
2020-04-30 11:37:06 -0400

    final commit

--- src/index.js
+++ src/index.js
@@ -13,9 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 let li = document.createElement('li');
                 li.textContent = comment.content;
                 commentsList.appendChild(li);
-                //call addCommentToDB to persist 
-                //only if we get there
-                //pass in each comment
             })
         })
     }
@@ -23,7 +20,6 @@ document.addEventListener('DOMContentLoaded', () => {
     //Click on the heart icon to increase image likes
     //still see them when I reload the page -> update db
     //click listener on like-button
-    let numLikes = 0; //starts as 0 anyway
     let likeBtn = document.querySelector('.like-button');
     likeBtn.addEventListener('click', event => {
         event.preventDefault();
@@ -31,26 +27,26 @@ document.addEventListener('DOMContentLoaded', () => {
         //first update DOM
         //gets the span that contains the likes
         let likes = document.querySelector('.likes');
-        //increase numLikes
-        numLikes += 1;
+        
         //put on DOM
+        let numLikes = showLikes();
+        // console.log(numLikes)
+        numLikes += 1;
         likes.textContent = `${numLikes} likes`;
         //then send to db
         //first get the likes from the server
         //then in that update them
         //should give access to the image object on server
-        fetch('http://localhost:3000/image')
-        .then(response => response.json())
-        .then(image => {
-            
-        })
+        
+        //gets the number of likes
+        
         fetch('http://localhost:3000/image', {
             method: "PATCH",
             headers : {
                 "accept": "application/json",
                 "content-type": "application/json"
             },
-            body: JSON.stringify(numLikes)
+            body: JSON.stringify({likes: numLikes})
         })
         .then(resp => resp.json())
         .then(console.log('updated'))
@@ -75,6 +71,21 @@ document.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    //will return the inital number of likes from db
+    function showLikes()
+    {
+        fetch('http://localhost:3000/image')
+        .then(response => response.json())
+        .then(image => {
+            let numLikes = image.likes;
+            console.log(image.likes)
+            let likes = document.querySelector('.likes');
+
+            likes.textContent = `${numLikes}`
+            // console.log(numLikes)
+            return numLikes;    
+        })
+    }
     //event listener for comments posted
     document.addEventListener('submit', event => {
         event.preventDefault();
@@ -85,10 +96,33 @@ document.addEventListener('DOMContentLoaded', () => {
         let newComment = commentForm.comment.value;
         let li = document.createElement('li');
         li.textContent = newComment;
+        // addComment(newComment); //adds to db
         commentsList.appendChild(li);
         commentForm.reset();
     })
 
+    //this function adds the comment to the db
+    // function addComment(comment)
+    // {
+
+    // }
+    
+    /*()
+    //TO WHOEVER READS THIS:
+    //i some how made likes = null in the db
+    //not sure how
+    //was trying to fix how i got the likes from the db
+    //of course this happened with 5 mins left
+    //am doing my best to fix it lol
+    //before i changed stuff it mostly worked
+    //i think it all worked in my last commit actually
+    //all that wasnt working is it wasnt gettting the likes from the db initially
+    //so it would always start at 0
+    //all i wanted to change was that but i somehow broke so much more 
+    //please have mercy on my soul
+    */ //forgot about block comments lmao
+
+    showLikes();
     showComments();
     getImage();
 })
