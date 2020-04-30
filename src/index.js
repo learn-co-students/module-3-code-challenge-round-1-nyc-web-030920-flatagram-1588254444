448793f3e324a6add974c43cea939a63985da18e
Eliot Henson <eliotrhenson@gmail.com>
2020-04-30 11:07:28 -0400

    deliverable 3

--- src/index.js
+++ src/index.js
@@ -14,40 +14,59 @@ document.addEventListener("DOMContentLoaded", () => {
         .then(showImages)
     }
 
-    // const showImages = images => {
-    //     let imageCard = document.getElementsByClassName("image-card")[0]
-    //     imageCard.innerHTML = ' '
-    //     console.log(imageCard)
+    const showImages = image => {
+        console.log(image)
+        let imageCard = document.getElementsByClassName("image-card")[0]
+        console.log(imageCard)
 
-    //     console.log(image.title)
+        const div = document.createElement('div')
 
-            // let card = `
-            // <h2 class="title">${image.title}</h2>
-            // <img src= ${image.image} class="image" />
-            // <div class="likes-section">
-            // <span class="likes"> ${image.likes}likes</span>
-            // <button class="like-button">♥</button>
-            // </div>
-            // <ul class="comments">
-            // <li> ${image.comments[0]} </li>
-            // <li> ${image.comments[1]} </li>
-            // <li> ${image.comments[2]} </li>
-            // </ul>
-            // <form class="comment-form">
-            // <input
-            // class="comment-input"
-            // type="text"
-            // name="comment"
-            // placeholder="Add a comment..."
-            // />
-            // <button class="comment-button" type="submit">Post</button>
-            // </form>
-            // `
-
-            // imageCard.append(card)
+            imageCard.innerHTML = `
+            <h2 class="title">${image.title}</h2>
+            <img src= ${image.image} class="image" />
+            <div class="likes-section">
+            <span class="likes"> ${image.likes}likes</span>
+            <button class="like-button">♥</button>
+            </div>
+            <ul class="comments">
+            <li> ${image.comments[0]} </li>
+            <li> ${image.comments[1]} </li>
+            <li> ${image.comments[2]} </li>
+            </ul>
+            <form class="comment-form">
+            <input
+            class="comment-input"
+            type="text"
+            name="comment"
+            placeholder="Add a comment..."
+            />
+            <button class="comment-button" type="submit">Post</button>
+            </form>
+            `
 
 
-    // }
+
+
+    }
+
+    document.addEventListener("click", (e) => {
+        e.preventDefault()
+        console.log(e.target.className === "like-button")
+       if (e.target.className === "like-button"){
+            let span= e.target.previousElementsSibling.innerText
+            console.log(span)
+            let likes = parseInt(span)
+            let newLikes = likes + 1
+       }
+    })
+
+    document.addEventListener('submit', e => {
+        e.preventDefault()
+        const commentForm = e.target
+
+        const comment = form.comment.value
+    
+    })
 
 
     getImages()
