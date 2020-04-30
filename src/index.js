5aaa990d7e88fda48ecb25ceb722b25262c8f31a
Kelsbecker <kelly.lynn.becker@gmail.com>
2020-04-30 10:57:17 -0400

    commit-two-new-approach

--- src/index.js
+++ src/index.js
@@ -15,34 +15,58 @@ const fetchImage = () => {
     .then(data => displayImage(data))
 };
 
-
 const displayImage = (data) => {
-    const imageContainer = document.querySelector('.image-container')
-    const commentUl = document.createElement('ul')
-    console.log(commentUl)
-    commentUl.className = 'comments'
-    const imageDiv = document.createElement('div')
-    imageDiv.className = 'image-card'
-    imageDiv.innerHTML = `
-    <h2 class="title">${data.title}</h2>
-    <img src=${data.image} class="image" />
-    <div class="likes-section">
-      <span class="likes">${data.likes} likes</span>
-      <button class="like-button">♥</button>
-    </div>
-    `
-    imageContainer.append(imageDiv)
-    data.comments.forEach(comment => displayComments(comment, commentUl))
-    imageContainer.append(commentUl)
+    const title = document.querySelector('.title')
+    title.textContent = data.title
+    const imageTag = document.querySelector('.image')
+    imageTag.src = data.image
+    const likesSpan = document.querySelector('.likes')
+    likesSpan.textContent = `${data.likes} Likes`
+
+    data.comments.forEach(comment => showComments(comment))
 };
 
-const displayComments = (comment, commentUl) => {
-    console.log(commentUl)
-    commentUl.innerHTML = `
-        <li>${comment.content}</li>
-    `
-    // const li = document.createElement('li')
-    // li.innerText = comment.content
-    // commentUl.append(li)
-};
+const showComments = (comment) => {
+
+    const commentContainer = document.querySelector('.comments')
+    let li = document.createElement('li') 
+    li.textContent = comment.content
+    commentContainer.append(li)
+}; 
+
+
+
+
+
+
+
+// const displayImage = (data) => {
+//     const imageContainer = document.querySelector('.image-container')
+//     const commentUl = document.createElement('ul')
+//     console.log(commentUl)
+//     commentUl.className = 'comments'
+//     const imageDiv = document.createElement('div')
+//     imageDiv.className = 'image-card'
+//     imageDiv.innerHTML = `
+//     <h2 class="title">${data.title}</h2>
+//     <img src=${data.image} class="image" />
+//     <div class="likes-section">
+//       <span class="likes">${data.likes} likes</span>
+//       <button class="like-button">♥</button>
+//     </div>
+//     `
+//     imageContainer.append(imageDiv)
+//     data.comments.forEach(comment => displayComments(comment, commentUl))
+//     imageContainer.append(commentUl)
+// };
+
+// const displayComments = (comment, commentUl) => {
+//     console.log(commentUl)
+//     commentUl.innerHTML = `
+//         <li>${comment.content}</li>
+//     `
+//     // const li = document.createElement('li')
+//     // li.innerText = comment.content
+//     // commentUl.append(li)
+// };
 
