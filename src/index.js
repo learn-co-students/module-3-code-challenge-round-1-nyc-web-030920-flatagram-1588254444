f03787aabb003abfcf9d743989bcf76503c517cf
RBRNFRVR <rommelchavez99@gmail.com>
2020-04-30 11:26:29 -0400

    work

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-// write your code here
+DOMImplementation
+
+let likes = document.querySelector('like-button').addEventListener("click");
+
+fetch('http://localhost:3000/image')
+    .then (response => response.json())
+    .then (likes => console.log(likes));
+ 
+
+
+
+ document.createElement('li')
+document.getElementsByClassName('comments').appendChild('ul li')
+
+
