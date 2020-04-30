7b3c0a61ca672dd5b33003b1bc8561255d38f719
JackyLin2 <jacky.jia.lin@gmail.com>
2020-04-30 11:38:38 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -3,28 +3,38 @@
 document.addEventListener("DOMContentLoaded", function(event){
  const requestHeader = { "Content-Type": "application/json", "Accept":"application/json"}   
  const imageCard = document.getElementsByClassName("image-card")[0]
+ likeButton()
  fetch("http://localhost:3000/image")
  .then((resp) => resp.json())
  .then((data) => {
      console.log(data)
-         let child = imageCard.childNodes
-         child[1].textContent = data.title
-         child[3].src = data.image
+         let inner = imageCard.childNodes
+         inner[1].textContent = data.title
+         inner[3].src = data.image
         comments()
         })
-         function likeButton(){
+
+
+
+        function likeButton(){
+            amountLikes = 0
          const likeSection = document.getElementsByClassName("like-button")[0]
-         let likeCount = document.getElementsByClassName("likes")[0]
         likeSection.addEventListener("click", function(event){
             event.preventDefault()
-            let newCount = parseInt(likeCount[0].innerHTML) + 1 
-            likeCount[0].innerHTML = newCount 
-            
+            let likeCount = document.getElementsByClassName("likes")[0]
+            likeCount.textContent = `${amountLikes} likes` 
+            amountLikes ++;
+            fetch("http://localhost:3000/image")
+            .then((resp) => resp.json())
+            .then(data => {
+                amountLikes = data.likes;
+            })
             fetch("http://localhost:3000/image", {
                 method:"PATCH",
                 headers: requestHeader,
-                body: JSON.stringify(`${newcount} likes`)
+                body: JSON.stringify({likes: amountLikes})
             })
+            .then((resp) => resp.json())
         })
      }
 
@@ -40,4 +50,29 @@ document.addEventListener("DOMContentLoaded", function(event){
         })
     })
    }
+function dogComment(){
+    let dogForm = document.getElementsByClassName("comment-form")
+    dogForm.addEventListener('submit', function(event){
+    event.preventDefault()
+    const form = event.target 
+    const comment = form.comment.value 
+    const newComment = {comment}
+
+    fetch("http://localhost:3000/image",{
+        method: "POST",
+        headers: requestHeader,
+        body: JSON.stringify(newComment)
+    })
+    .then((resp) => resp.json())
+    .then((comment) => {
+  addComment(comment)
+    })
+})
+}
+   
+function addComment(comment){
+    let p = document.createElement("p")
+            p.innerHTML = `${comment.content}` 
+            showComment.appendChild(p)
+}
 })
