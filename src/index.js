744af32f3172fe5b3627cc1e5d73757355b930f4
Josephscha <Josephshincha@gmail.com>
2020-04-30 11:35:38 -0400

    Could not figure out why fetch patch & post were not working, possibly something to do with the body format

--- src/index.js
+++ src/index.js
@@ -12,8 +12,8 @@
 
 document.addEventListener(`DOMContentLoaded`, () => {
     const baseUrl = "http://localhost:3000/image"
-    const reqHeaders = { "Content-Type": "application/json",
-"Accept": "application/json"}
+    const requestHeaders = { "Content-Type": "application/json",
+    "Accept": "application/json"}    
     const imageLocation = document.getElementsByClassName("image")[0]
     const commentLocation = document.getElementsByClassName("comments")[0]
     const titleLocation = document.querySelector(".title")
@@ -22,51 +22,67 @@ document.addEventListener(`DOMContentLoaded`, () => {
     const commentInput = document.querySelector(".comment-input")
     const commentForm = document.querySelector(".comment-form")
     const commentBtn = commentForm.querySelector(".comment-button")
-    
+    const likesSection = document.querySelector(".likes-section")
+    const downBtn = document.createElement("button")
+    downBtn.innerText = "downvote"
+    likesSection.append(downBtn)
+
     const getImage = () => {
         fetch(baseUrl)
         .then(response => response.json())
         .then(image => renderImage(image))
-    }
+    }    
 
     function renderImage(image){
         imageLocation.src = image.image
         renderComments(image)
         renderLikes(image)
         renderTitle(image)
-    }
+    }    
 
-    function renderComments(source){
+        const deleteBtn = document.createElement("button")
+        deleteBtn.textContent = "Delete"
+    
+        function renderComments(source){
         const comments = commentLocation.childNodes
         comments[1].textContent = source.comments[0].content
         comments[3].textContent = source.comments[1].content
         comments[5].textContent = source.comments[2].content
-    }
+    }    
 
     function renderLikes(source){
         likes.textContent = `${source.likes} likes`
-    }
+    }    
 
     function renderTitle(source){
         titleLocation.textContent = source.title
-    }
+    }    
 
-    likeBtn.addEventListener("click", event => {
-        const newCount = parseInt(likes.textContent) + 1 
-        likes.textContent = `${newCount} likes`
-        patchLikes(newCount)
-    })
+    likesSection.addEventListener("click", event => {
+        if(event.target === likeBtn){
+            const newCount = parseInt(likes.textContent) + 1     
+            likes.textContent = `${newCount} likes`
+            patchLikes(likes)
+        }
+        else if (event.target === downBtn){
+            const newCount = parseInt(likes.textContent) - 1
+            likes.textContent = `${newCount} likes`
+            patchLikes(likes)
+        }   
+        else null 
+    })    
 
+    //not persisting to database. I believe I'm formatting the body wrong. 
     function patchLikes(newCount){        
         fetch("http://localhost:3000/image"), {
             method: "PATCH",
-            headers: reqHeaders,
+            headers: requestHeaders,
             body: JSON.stringify({
-            "likes": `${newCount}`
+            "likes": `${newCount}`    
             })
-        }
-    }       
-    
+        }    
+    }           
+
     commentForm.addEventListener("submit", event => {
         event.preventDefault()
         let comment = commentInput.value 
@@ -74,14 +90,15 @@ document.addEventListener(`DOMContentLoaded`, () => {
         newComment.textContent = comment
         commentLocation.append(newComment)
         commentForm.reset()
-        // fetch(baseUrl), {
-        //     method: "POST",
-        //     headers: reqHeaders,
-        //     body: JSON.stringify({
-        //         "comments": `${comment}`
-        //     })
-        // }
-    })
-    
+        //Not persisting to database.
+        fetch(baseUrl), {
+            method: "POST",    
+            headers: requestHeaders,
+            body: JSON.stringify({
+                "comments": `${comment}`    
+            })
+        }
+    })    
+
     getImage()
 })
